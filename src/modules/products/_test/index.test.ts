f08b38857ac91c3623b4c1108c9201d1219c7085
keeptrain <chefgilang2@gmail.com>
2026-08-27 20:53:22 +0700

    feat(products): add products module
    - add GET /products with randomUUIDv7 and mock data
    - add products test with native APP_URL fallback

---
+++ src/modules/products/_test/index.test.ts
@@ -0,0 +1,16 @@
+// test/index.test.ts
+import { describe, expect, it } from 'bun:test';
+import { testApp } from '../../..';
+
+const url = `${process.env.APP_URL ?? 'http://localhost:3000'}/products`;
+
+describe('products/index Controller', () => {
+  it('returns a response', async () => {
+    console.log(url);
+
+    // const data = await response.json();
+    // expect(data).toHaveProperty('data');
+    // expect(Array.isArray(data.data)).toBe(true);
+    // expect(data.data.length).toBeGreaterThan(0);
+  });
+});
---
+++ src/modules/products/index.ts
@@ -0,0 +1,20 @@
+import { randomUUIDv7 } from 'bun';
+import Elysia from 'elysia';
+
+export const productsApp = new Elysia().get('/products', () => {
+  const products = [
+    {
+      id: randomUUIDv7(),
+      name: 'Product 1',
+      price: 10.99,
+      description: 'This is product 1',
+    },
+    {
+      id: randomUUIDv7(),
+      name: 'Product 2',
+      price: 19.99,
+      description: 'This is product 2',
+    },
+  ];
+  return { data: products };
+});
