64403f1420ccb4ea7771daa1a155a8acbd098b62
keeptrain <chefgilang2@gmail.com>
2026-09-16 16:26:51 +0700

    refactor(auth): move betterAuthView + RELEVANT_AUTH_PATHS from utils to modules/auth/utilts
    - Delete src/utils/auth-utils.ts
    - Create src/modules/auth/utilts.ts with GMAIL_ONLY, RELEVANT_AUTH_PATHS, betterAuthView, betterAuthRouteHook
    - Update import paths in src/index.ts and src/utils/openapi-utils.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from 'elysia';
 
 import { isProduction, env } from '@/utils/env';
-import { betterAuthRouteHook, betterAuthView } from '@/utils/auth-utils';
+import { betterAuthRouteHook, betterAuthView } from '@/modules/auth/utilts';
 import { corsPlugin, openapiPlugin } from '@/lib/elysia-plugins';
 import { productsApp } from '@/modules/products';
 import { ipRateLimiterMiddleware } from '@/middleware/public-middleware';
